09dfc0349cd47e216ef29cf8fcebccce96d124e9
張 建文 <chang180@gmail.com>
2020-08-04 21:02:47 +0800

    前台預告片動畫輪播完成，請務必記熟

--- front/main.php
+++ front/main.php
@@ -4,7 +4,7 @@
         <div class="rb tab" style="width:95%;">
             <div id="abgne-block-20111227">
                 <!-- 海報區 -->
-                <div class="poster" style="height:300px">
+                <div class="poster" style="height:300px;position:relative">
                     <img id="showpost" style="height:250px"><br>
                     <span id="showname"></span>
                 </div>
@@ -15,9 +15,9 @@
                     $posters = $Poster->all(['sh' => 1], " ORDER BY rank DESC ");
                     foreach ($posters as $key => $p) {
                     ?>
-                        <div class="im" id="ssaa<?= $key; ?>" onclick="change(<?=$key;?>)"><img src="img/<?= $p['path']; ?>" style="width:60px;height:80px"><br>
-                    <div class="txt"><?=$p['movie'];?></div>
-                    </div>
+                        <div class="im" id="ssaa<?= $key; ?>" onclick="change(<?= $key; ?>)"><img src="img/<?= $p['path']; ?>" style="width:60px;height:80px"><br>
+                            <div class="txt"><?= $p['movie']; ?></div>
+                        </div>
                     <?php
                     }
                     ?>
@@ -42,12 +42,13 @@
 <script>
     var nowpage = 0,
         num = <?= count($posters); ?>;
+
     function pp(x) {
         var s, t;
         if (x == 1 && nowpage - 1 >= 0) {
             nowpage--;
         }
-        if (x == 2 && (nowpage + 1) * 3 <= num * 1 + 3) {
+        if (x == 2 && (nowpage + 1)  <= num * 1 - 4) {
             nowpage++;
         }
         $(".im").hide()
@@ -58,12 +59,50 @@
     }
     pp(1)
 
-    let anim=<?=$_SESSION['ani'];?>;
-    
-    // 先放上海報和片名
-function change(post){
-    $("#showpost").attr("src",$("#ssaa"+post).find("img").attr("src"));
-    $("#showname").text($("#ssaa"+post).find(".txt").text());
-}
+    let anim = <?= $_SESSION['ani']; ?>;
+    let po = 0;
 
+    // 先放上海報和片名
+    function change(post) {
+        $("#showpost").attr("src", $("#ssaa" + post).find("img").attr("src"));
+        $("#showname").text($("#ssaa" + post).find(".txt").text());
+        po = post;
+    }
+    // 把第一張先放上
+    change(0);
+
+    // 設定時間用動畫換頁，
+    setInterval(() => {
+        po++;
+        if (po == num) po = 0;
+        ani();
+    }, 3000);
+
+    //動畫函式，先出場，再換圖文，再進場(真正重點，記不住就GG了)
+    // 函式只是指定進出場的動作，不需要使用參數
+    function ani() {
+        switch (anim) {
+            case 1: //縮放
+                $(".poster").fadeOut();
+                change(po);
+                $(".poster").fadeIn();
+                break;
+            case 2: //放大縮小
+                $(".poster").slideToggle();
+                change(po);
+                $(".poster").slideToggle();
+                break;
+            default: //移出移入，動畫位置div裏的relative一定要設定
+                $(".poster").animate({
+                    left: "-=200px",
+                    opacity: "0"
+                });
+                change(po);
+                $(".poster").animate({
+                    left: "+=200px",
+                    opacity: "1"
+                });
+                break;
+        }
+    }
 </script>
